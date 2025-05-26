32fa5a40d38c9ef3b7514e6128b2a2083d4e31c4
las_r <128936184+las-r@users.noreply.github.com>
2025-05-25 21:30:43 -0400

    Update loading-circle.py

--- examples/loading-circle.py
+++ examples/loading-circle.py
@@ -2,9 +2,14 @@ import max7219
 from mtxanim import Anim, strToFrame
 from machine import Pin, SPI
 
+# example for matrix-anim
+# this should make a loading circle
+
+# lcd matrix
 spi = SPI(1, baudrate=10000000, polarity=0, phase=0, sck=Pin(18), mosi=Pin(23))
 cs = Pin(5, Pin.OUT)
 
+# define frames using str to frame func
 frames = [
     strToFrame("..####../.#....#./#......./#......./#......./#......./.#....#./..####../"),
     strToFrame("..####../.#....#./#......#/#......#/#......#/#......#/.#....#./......../"),
@@ -12,4 +17,5 @@ frames = [
     strToFrame("......../.#....#./#......#/#......#/#......#/#......#/.#....#./..####../")
 ]
 
+# create and play animation
 Anim(frames, max7219.Matrix8x8(spi, cs, 1)).play(8)
